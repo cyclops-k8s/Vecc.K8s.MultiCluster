d4cba4f91e01b04c18af07e026ad17d50172971a
Edward Cooke <edward@frakkingsweet.com>
2024-09-04 11:41:23 -0600

    Try and make the test work on github agents

--- test/create-docker-registry.sh
+++ test/create-docker-registry.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-set -e
-
 if [ "$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
   docker run \
     -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
     registry:2 | docker start "${reg_name}"
 fi
+
+docker container ps
+docker logs "${reg_name}"
