d334cf5c114a68b438888bd7750da8cd34f4a2ac
Ed Cooke <ecooke@macu.com>
2025-02-20 10:38:43 -0700

    Use a different dns server, testing the delay

--- src/Vecc.K8s.MultiCluster.Api/Services/Default/DefaultDnsHost.cs
+++ src/Vecc.K8s.MultiCluster.Api/Services/Default/DefaultDnsHost.cs
@@ -1,44 +1,23 @@
-﻿using Vecc.Dns.Server;
+﻿using ARSoft.Tools.Net.Dns;
 
 namespace Vecc.K8s.MultiCluster.Api.Services.Default
 {
     public class DefaultDnsHost : IDnsHost
     {
         private readonly ILogger<DefaultDnsHost> _logger;
-        private readonly IDnsServer _dnsServer;
-        private readonly CancellationToken _shutdownCancellationToken;
+        private readonly DnsServer _dnsServer;
 
-        public DefaultDnsHost(ILogger<DefaultDnsHost> logger, IDnsServer dnsServer, IHostApplicationLifetime lifetime)
+        public DefaultDnsHost(ILogger<DefaultDnsHost> logger, DnsServer dnsServer)
         {
             _logger = logger;
             _dnsServer = dnsServer;
-            _shutdownCancellationToken = lifetime.ApplicationStopping;
         }
 
-        public async Task StartAsync()
+        public Task StartAsync()
         {
-            while (!_shutdownCancellationToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Starting dns server");
-
-                try
-                {
-                    await _dnsServer.ExecuteAsync(_shutdownCancellationToken);
-
-                    if (!_shutdownCancellationToken.IsCancellationRequested)
-                    {
-                        _logger.LogError("Unexpected shutdown of the dns server, restarting.");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    if (!_shutdownCancellationToken.IsCancellationRequested)
-                    {
-                        _logger.LogError(ex, "Crash in the dns server, restarting.");
-                    }
-                }
-            }
-            _logger.LogInformation("Shutdown of dns server complete");
+            _logger.LogInformation("Starting dns server");
+            _dnsServer.Start();
+            return Task.CompletedTask;
         }
     }
 }
---
+++ src/Vecc.K8s.MultiCluster.Api/Services/IDnsResolver.cs
@@ -0,0 +1,11 @@
+﻿using ARSoft.Tools.Net.Dns;
+
+namespace Vecc.K8s.MultiCluster.Api.Services
+{
+    public interface IDnsResolver
+    {
+        OnHostChangedAsyncDelegate OnHostChangedAsync { get; }
+        Task InitializeAsync();
+        Task<DnsMessage> ResolveAsync(DnsMessage message);
+    }
+}
